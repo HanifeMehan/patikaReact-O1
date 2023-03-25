9a638a52c9e399d3ff4bfe650bea6bc9800175ba
HanifeMehan <hanifemehan0853@gmail.com>
2023-03-26 00:26:57 +0300

    getData

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+async function getData(id){
+  const {data: users} = await axios("https://jsonplaceholder.typicode.com/users/"+id);
+  const {data: posts}= await axios("https://jsonplaceholder.typicode.com/posts?id="+id);
+
+  console.log("USERS:",users);
+  console.log("POSTS:",posts);
+
+}
+export default getData;
