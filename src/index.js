4656ee28ef91dbf16145be07b702111bf3766774
HanifeMehan <hanifemehan0853@gmail.com>
2023-03-26 00:48:30 +0300

    comment line

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 async function getData(id){
+  //Axios calls made
   const {data: users} = await axios("https://jsonplaceholder.typicode.com/users/"+id);
   const {data: posts}= await axios("https://jsonplaceholder.typicode.com/posts?id="+id);
 
